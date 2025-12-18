0db744cb4024a4e91f0381fe17ca78dc3b23bab5
Alden Pierre <Alden.Pierre@gmail.com>
2025-12-18 15:28:11 -0500

    get_num_chars function added to stats.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def main():
     fd  = "books/frankenstein.txt"
     text = get_book_text(fd)
     number_of_words = get_num_words(text)
+    myDict = get_num_chars(text)
     msg = f"Found {number_of_words} total words"
+    
     print(msg)
+    print(myDict)
+
  
 def get_book_text(path):
     file_content = None
--- stats.py
+++ stats.py
@@ -5,3 +5,12 @@ def get_num_words(book_contents):
     for words in book_contents_split:
         counter += 1
     return counter
+
+def get_num_chars(book_contents):
+    dictionaryCounter = {}
+    for char in book_contents:
+        if char in dictionaryCounter:
+            dictionaryCounter[char] += 1
+        else:
+            dictionaryCounter[char] = 1
+    return dictionaryCounter
