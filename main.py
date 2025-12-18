da7810b05bae205e825af96e2e032dab0cf26c3b
Alden Pierre <Alden.Pierre@gmail.com>
2025-12-18 12:05:18 -0500

    get_book_words function added

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 def main():
     fd  = "books/frankenstein.txt"
     text = get_book_text(fd)
+    number_of_words = get_book_words(text)
+    msg = f"Found {number_of_words} total words"
+    print(msg)
  
 def get_book_text(path):
+    file_content = None
     with open(path) as f:
-        print(f.read())
+        file_content = f.read()        
+    return file_content
+
+def get_book_words(book_contents):
+    book_contents_split = book_contents.split()
+    counter = 0
+
+    for words in book_contents_split:
+        counter += 1
+    return counter
+
 
 main()
